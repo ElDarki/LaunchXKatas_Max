90ae03be3c949e52a13b7b8154f0122ae9d3d2ee
ElDarki <darkar.zero117@gmail.com>
2022-02-11 20:15:41 -0700

    Subiendo Katas 1 y 3

---
+++ Modulo1Katas.py
@@ -0,0 +1,9 @@
+from datetime import date
+date.today()
+print(date.today())
+
+print("Hoy es: " + str(date.today()))
+
+print("Bienvenido a la grieta del invocador")
+name = input("Como te llamas? ")
+print("Salutations " + name)
